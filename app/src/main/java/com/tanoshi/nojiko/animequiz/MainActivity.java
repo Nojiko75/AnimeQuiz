7abb3bdce6bb75c0f22a986c2bc66ffa423a2372
Nojiko75 <mkpi.julie@gmail.com>
2017-01-14 20:13:20 +0100

    Home menu work

--- app/src/main/java/com/tanoshi/nojiko/animequiz/MainActivity.java
+++ app/src/main/java/com/tanoshi/nojiko/animequiz/MainActivity.java
@@ -1,13 +1,49 @@
 package com.tanoshi.nojiko.animequiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button game_btn;
+    private Button ranking_btn;
+    private Button quit_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.game_btn = (Button) findViewById(R.id.game_btn);
+        this.ranking_btn = (Button) findViewById(R.id.ranking_btn);
+        this.quit_btn = (Button) findViewById(R.id.quit_btn);
+
+        this.game_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        this.ranking_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RankingActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        this.quit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
